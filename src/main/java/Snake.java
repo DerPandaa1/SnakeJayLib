12da5383a317c7d103cb48fd4596a8c54ec9d43c
Jannis <derpandaa6@gmail.com>
2024-01-23 10:43:55 +0100

    Positionen erstellt und gezeichnet
    Zuerst wurden Koordinaten auf dem Spielfeld herausgesucht, auf die später verwiesen wird. An diesen Punkten wird danach jeweils ein Quadrat gezeichnet.
    Außerdem wird eine zusätzliche Zufällige Koordinate generiert, an der ein rotes Quadrat gezeichnet wird.

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -1,22 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+//FeldGröße 32*18
+
 
 /**
  * @author <a href="mailto:Alexander.Rathai@Materna.DE">Alexander Rathai</a>
  *
  */
 public class Snake {
+	List<Pos> snake = new ArrayList<Pos>();
+	Pos posApple = new Pos(-1, -1);
 	public Snake() {
 		
 	}
 	public void init() {
-		
+		snake.add(new Pos(7, 7));
+		snake.add(new Pos(8, 7));
+		int xRandom = getRandomInt(0,32);
+		int yRandom = getRandomInt(0,18);
+		posApple.x = xRandom;
+		posApple.y = yRandom; 
+//		snake.add(new Pos(xRandom, yRandom));
 	}
 	public void loop() {
-//		PandaaHelper.drawTile(1,5);	
+		
+		Pos posA = snake.get(0);
+		int xA= posA.x;
+		int yA = posA.y;
+		
+		PandaaHelper.drawTile(xA, yA);
+		
+
+		Pos posB= snake.get(1);
+		int xB = posB.x;
+		int yB = posB.y; 
+		PandaaHelper.drawTile(xB, yB);
+		
+//		Pos posApple = snake.get();
+		int xC = posApple.x; 
+		int yC = posApple.y; 
+		PandaaHelper.drawApple(xC, yC);
 	}
 	private void Move(String direction) {
 		//TODO
 	}
-	private boolean isColliding(PandaaHelper.Pos posA, PandaaHelper.Pos posB) {
+	private boolean isColliding(Pos posA, Pos posB) {
 		//TODO
+	}
+	private int getRandomInt(int min, int max) {
+		int diff= max-min;
+		return Math.round(Math.round(Math.random()*diff +min));
+	}
+
+	public class Pos{
+		int x;
+		int y;
+		public Pos(int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
 	}
 }
